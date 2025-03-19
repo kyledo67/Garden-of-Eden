6430213a22a73c0ca8d5fbbf441f30d695abbfc9
kyledo67 <127157832+kyledo67@users.noreply.github.com>
2025-03-19 13:52:12 -0500

    Update button.js

--- button.js
+++ button.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', function() {
     const toggleOptions = document.getElementById('toggleOptions');
     const optionsMenu = document.getElementById('optionsMenu');
